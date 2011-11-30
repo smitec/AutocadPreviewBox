6eff9f07baece20ff984e811c1afbdc684838adc
Elliot <elliot.smith@ausenco.com.au>
2011-11-30 13:49:24 +1000

    initial commit.
    contains some hacked together code that needs to be formalized into a
    working and useful user control.
    
    - Elliot

---
+++ UserControl1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace autocadhelloworld
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void load_file_preview(string filename)
+        {
+
+        }
+    }
+}
