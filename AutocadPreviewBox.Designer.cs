d488a821f14c397490d6c9aa8042d6621be44e57
Elliot Smith <elliot.smith91@gmail.com>
2011-12-06 09:40:11 +1000

    separated getting and setting the image to allow the image to be returned. Added a passthough on click event to the image to allow the user control OnClick to be used.

--- AutocadPreviewBox.Designer.cs
+++ AutocadPreviewBox.Designer.cs
@@ -41,6 +41,7 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // PreviewBox
             // 
--- AutocadPreviewBox.cs
+++ AutocadPreviewBox.cs
@@ -39,6 +39,7 @@ namespace previewer
         /// <param name="e">event arguments</param>
         private void UserControl1_Load(object sender, EventArgs e)
         {
+            //dont know if this is actually needed.
             this.pictureBox1.Width = this.Width - 3;
             this.pictureBox1.Height = this.Height - 3;
         }
@@ -50,6 +51,28 @@ namespace previewer
         /// <param name="resize">Should the box resize to the new image</param>
         public void load_file_preview(string filename, bool resize = false)
         {
+           
+            System.Drawing.Bitmap preview = get_image_of_file(filename);
+
+            assign_image(preview, resize);
+        }
+
+        public void assign_image(System.Drawing.Bitmap newImage, bool resize = false)
+        {
+            //place the picture in the preview box and resize
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.pictureBox1.Image = newImage;
+
+            //resize the picture box (not the pallete) if it was asked for
+            if (resize == true)
+            {
+                this.pictureBox1.Width = newImage.Width;
+                this.pictureBox1.Height = newImage.Height;
+            }
+        }
+
+        public System.Drawing.Bitmap get_image_of_file(string filename)
+        {
             //create a new database instance and load the dwg file into it.
             Database dbb = new Database(false, true);
             dbb.ReadDwgFile(filename, FileOpenMode.OpenForReadAndAllShare, false, "");
@@ -57,23 +80,19 @@ namespace previewer
             //grab the thumbnail bitmap and get rid of the white background
             System.Drawing.Bitmap preview = dbb.ThumbnailBitmap;
             preview.MakeTransparent(System.Drawing.Color.White);
-                
-            //place the picture in the preview box and resize
-            this.pictureBox1.BackColor = System.Drawing.Color.LightSlateGray;
-            this.pictureBox1.Image = preview;
-            
-            //resize the picture box (not the pallete) if it was asked for
-            if (resize == true)
-            {
-                this.pictureBox1.Width = preview.Width;
-                this.pictureBox1.Height = preview.Height;
-            }
+
+            return preview;
         }
 
         private void PreviewBox_Resize(object sender, EventArgs e)
         {
             this.pictureBox1.Width = this.Width - 3;
             this.pictureBox1.Height = this.Height - 3;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.OnClick(e);
         }
     }
 }
